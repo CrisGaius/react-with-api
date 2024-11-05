d04eee7accd6a0b27ec4a2ea0b625a3389dbad12
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-11-04 21:50:44 -0300

    Added the funcionality of show all students in database

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom/";
+
+import { get } from "lodash";
+import { FaUserCircle, FaEdit, FaWindowClose } from "react-icons/fa";
 
 import { Container } from "../../styles/GlobalStyles";
+import { ProfilePicture, StudentContainer } from "./styled";
+
 import axios from "../../services/axios";
 
 export default function Students() {
-  React.useEffect(() => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
     async function getData() {
       const response = await axios.get("/students");
-      console.log(response.data);
+      setStudents(response.data);
     }
 
     getData();
@@ -15,6 +23,30 @@ export default function Students() {
   return (
     <Container>
       <h1>Students</h1>
+      <StudentContainer>
+        {students.map((student) => (
+          <div key={String(student.id)}>
+            <ProfilePicture>
+              {get(student, "StudentPhotos[0].url", false) ? (
+                <img src={student.StudentPhotos[0].url} alt="Profile" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <span>{student.name}</span>
+            <span>{student.email}</span>
+
+            <Link to={`/student/${student.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+
+            <Link to={`/student/${student.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </StudentContainer>
     </Container>
   );
 }
