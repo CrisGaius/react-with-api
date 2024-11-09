fa38d9d3019d8ec8b9634d07e88fae71c6ffea93
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-11-08 23:25:44 -0300

    implementing sagas.js for authentication

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,10 +1,19 @@
-import { all, takeLatest } from "redux-saga/effects";
-// import { toast } from "react-toastify";
-// import * as actions from "./actions";
+import { put, all, takeLatest, call } from "redux-saga/effects";
+import { toast } from "react-toastify";
+import * as actions from "./actions";
 import * as types from "../types";
+import axios from "../../../services/axios";
 
 function* loginRequest({ payload }) {
-  console.log("SAGA", payload);
+  try {
+    const response = yield call(axios.post, "/tokens", payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.successs("Success to sign in.");
+  } catch (e) {
+    toast.error("User or password is wrong.");
+    yield put(actions.loginFailure());
+  }
 }
 
 export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
