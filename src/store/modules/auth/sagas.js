aaa3e521396cf682f9902ecf41d2367f334b12a8
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-11-09 23:53:55 -0300

    Finished login of users in application

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,19 +1,35 @@
 import { put, all, takeLatest, call } from "redux-saga/effects";
 import { toast } from "react-toastify";
+import { get } from "lodash";
 import * as actions from "./actions";
 import * as types from "../types";
 import axios from "../../../services/axios";
+import history from "../../../services/history";
 
 function* loginRequest({ payload }) {
   try {
     const response = yield call(axios.post, "/tokens", payload);
     yield put(actions.loginSuccess({ ...response.data }));
 
-    toast.successs("Success to sign in.");
+    toast.success("Success to sign in.");
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
   } catch (e) {
     toast.error("User or password is wrong.");
     yield put(actions.loginFailure());
   }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+function persistRehydrate({ payload }) {
+  const token = get(payload, "auth.token", "");
+
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
